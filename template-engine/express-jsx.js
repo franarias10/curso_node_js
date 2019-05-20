5f5bd8fa9927ed4e9bad52316a1244c15e773d6f
Francisco Arias <fco.arias10@gmail.com>
2019-05-20 13:12:57 -0400

    implementacion de custom template engine
    implementacion de un custom template engine en express

---
+++ template-engine/express-jsx.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+
+function getKeysFromOptions(options){
+    const {settings, _locals, ...objectKeys} = options
+    return  Object.keys(objectKeys)
+}
+
+function getRenderedContent(content, options){
+    const keys = getKeysFromOptions(options)
+   
+    let contentString = content.toString()
+    
+    for(let key of keys){
+        console.log(keys)
+        contentString = contentString.replace(
+            new RegExp(`\{${key}\}`, 'gi'),
+            options[key]
+        )
+    }
+
+    return contentString
+}
+function expressJsx(filePath, options, callback){
+    fs.readFile(filePath, function(err, content){
+        if(err){
+            return callback(err)
+        }
+        
+        const rendered = getRenderedContent(content, options)
+
+        return callback(null, rendered)
+    })
+}
+
+module.exports = expressJsx 
---
+++ template-engine/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const expressJsx = require('./express-jsx')
+
+
+
+app.engine('jsx', expressJsx)
+app.set('views', './views')
+app.set('view engine', 'jsx')
+
+app.get('/',function(req, res){
+    res.render('index', {hello: 'hola', world: 'mundo'})
+})
+
+const server = app.listen(8000, function(){
+    console.log(`listening http://localhost:${server.address().port}`)
+})
